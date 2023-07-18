befd18b2b0820ffad1315154fc806f357c227f74
TeerapongP <thirapongp7@gmail.com>
2023-07-18 22:45:37 +0700

    add seach

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server');
 const clone = require('clone');
-const cors = require("cors");
+const cors = require('cors');
 const data = require('./db.json');
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
@@ -8,6 +8,7 @@ const port = process.env.PORT || 8000;
 
 const createServer = () => {
     const server = jsonServer.create();
+    server.use(cors());
     const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
         _isFake: isProductionEnv,
     });
@@ -26,6 +27,22 @@ const createServer = () => {
 
     return server;
 };
+
+server.get('/search', (req, res) => {
+    const tags = req.query.tags;
+
+    if (!tags || !Array.isArray(tags) || tags.length === 0) {
+        res.status(400).json({ error: 'Invalid or empty tags parameter' });
+        return;
+    }
+
+    const filteredData = data.filter(item => {
+        return item.tags.some(tag => tags.includes(tag));
+    });
+
+    res.json(filteredData);
+});
+
 
 const startServer = (server, port) => {
     server.listen(port, () => {
